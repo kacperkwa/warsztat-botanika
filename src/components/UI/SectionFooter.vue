db4aba038808e67ea826bc6dbe8fae9bcabe8912
kacperkwa <kacperkwartaxz@gmail.com>
2024-08-24 10:18:39 +0200

    added about me section

--- src/components/UI/SectionFooter.vue
+++ src/components/UI/SectionFooter.vue
@@ -8,17 +8,22 @@
 div {
   display: flex;
   justify-content: center;
-  background-color: var(--section-footer-background-color);
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
   padding: 1rem;
+  background-color: var(--section-footer-background-color);
+  color: var(--text-color-primary);
   text-transform: uppercase;
 }
 p {
   margin: 0;
-  padding: 0 1rem; /* Równy padding z obu stron */
+  padding: 0 1rem;
   font-size: 1.2rem;
 }
 
 p:first-child {
-  border-right: 1px solid var(--text-color-primary); /* Linia pionowa */
+  border-right: 1px solid var(--text-color-primary);
 }
 </style>
--- src/components/layout/AboutMe.vue
+++ src/components/layout/AboutMe.vue
@@ -23,8 +23,8 @@
         <img class="aboutme-img" src="../../assets/img/aboutme-image.webp" alt="About me image," />
       </div>
     </div>
+    <SectionFooter></SectionFooter>
   </section>
-  <SectionFooter></SectionFooter>
 </template>
 <script setup>
 import SectionFooter from '@/components/UI/SectionFooter.vue'
@@ -33,17 +33,19 @@ import SectionFooter from '@/components/UI/SectionFooter.vue'
 #aboutme {
   display: flex;
   align-items: center;
+  justify-content: center;
+  padding: 2rem 2rem 6rem 2rem;
   height: 100vh;
   background-color: var(--background-color-secondary);
   color: var(--text-color-secondary);
+  position: relative;
 }
 .container {
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  justify-content: space-between;
   width: 100%;
   max-width: 1200px;
-  margin: 0 auto;
 }
 .text-container {
   display: flex;
@@ -53,18 +55,39 @@ import SectionFooter from '@/components/UI/SectionFooter.vue'
 }
 h2 {
   text-transform: uppercase;
-  font-size: 5rem;
+  font-size: calc(2.8rem + 1vw);
   margin-bottom: 2rem;
 }
 p {
   display: flex;
   flex-direction: column;
   gap: 3rem;
-  font-size: 1.8rem;
+  font-size: calc(1.2rem + 0.5vw);
   line-height: 2.4rem;
 }
 .image-container {
   width: 100%;
   max-width: 500px;
+}
+@media (max-width: 768px) {
+  #aboutme {
+    flex-direction: column;
+    height: auto;
+  }
+  .container {
+    flex-direction: column;
+    gap: 2rem;
+  }
+  .text-container {
+    padding: 1rem;
+  }
+  h2 {
+    font-size: calc(2rem + 1vw);
+  }
+  p {
+    font-size: calc(1rem + 0.5vw);
+    line-height: 2rem;
+    gap: 1rem;
+  }
 }
 </style>
